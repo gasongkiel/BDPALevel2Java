78516c17938df3f05454ec890fdb65d263d8b8da
unknown <BDPA14@metrostate.edu>
2018-02-24 14:05:57 -0600

    hi

--- super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
+++ super-hero-page/src/main/java/com/example/superheropage/SuperHeroController.java
@@ -8,13 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SuperHeroController {
     @RequestMapping("/superhero")
     public String greeting(Model model) {
-        model.addAttribute("name", "Super Hero Name");
-        model.addAttribute("power1", "A power that is super");
-        model.addAttribute("power2", "Another power that is super");
-        model.addAttribute("power3", "A third power that is super");
-        model.addAttribute("fact1", "This is fun fact is maybe not so fun");
-        model.addAttribute("fact2", "This is another fun fact");
-        model.addAttribute("fact3", "This is the third ");
+        model.addAttribute("name", " cabbage man");
+        model.addAttribute("power1", "supper cabbage");
+        model.addAttribute("power2", "cabbage growth");
+        model.addAttribute("power3", "red cabbage");
+        model.addAttribute("fact1", "cabbage is an exellent source of vitamin K, C, and B6");
+        model.addAttribute("fact2", "cabbage man is trying to lose weight");
+        model.addAttribute("fact3", "cabbage man is nearsighted");
 
         return "hero"; //Return "hero" to tell spring to use the hero.html template
     }
